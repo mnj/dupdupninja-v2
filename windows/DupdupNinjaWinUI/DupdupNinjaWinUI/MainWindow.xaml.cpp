e690c18890d6be83ea060058a03798b04862a151
mij <michael@sys-adm.dk>
2025-12-27 14:48:06 +0100

    Add Settings/About dialogs to WinUI app

--- windows/DupdupNinjaWinUI/DupdupNinjaWinUI/MainWindow.xaml.cpp
+++ windows/DupdupNinjaWinUI/DupdupNinjaWinUI/MainWindow.xaml.cpp
@@ -3,9 +3,32 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
 
 namespace winrt::DupdupNinjaWinUI::implementation {
 MainWindow::MainWindow() {
     InitializeComponent();
+}
+
+void MainWindow::Settings_Click(IInspectable const&, RoutedEventArgs const&) {
+    ContentDialog dialog;
+    dialog.Title(box_value(L"Settings"));
+    dialog.Content(box_value(L"Settings are not implemented yet."));
+    dialog.CloseButtonText(L"Close");
+    dialog.XamlRoot(this->Content().XamlRoot());
+    dialog.ShowAsync();
+}
+
+void MainWindow::About_Click(IInspectable const&, RoutedEventArgs const&) {
+    ContentDialog dialog;
+    dialog.Title(box_value(L"About dupdupninja"));
+    dialog.Content(box_value(L"Cross-platform duplicate/near-duplicate media finder."));
+    dialog.CloseButtonText(L"Close");
+    dialog.XamlRoot(this->Content().XamlRoot());
+    dialog.ShowAsync();
+}
+
+void MainWindow::Exit_Click(IInspectable const&, RoutedEventArgs const&) {
+    Close();
 }
 }
--- windows/DupdupNinjaWinUI/DupdupNinjaWinUI/MainWindow.xaml.h
+++ windows/DupdupNinjaWinUI/DupdupNinjaWinUI/MainWindow.xaml.h
@@ -5,6 +5,10 @@
 namespace winrt::DupdupNinjaWinUI::implementation {
 struct MainWindow : MainWindowT<MainWindow> {
     MainWindow();
+
+    void Settings_Click(IInspectable const&, Microsoft::UI::Xaml::RoutedEventArgs const&);
+    void About_Click(IInspectable const&, Microsoft::UI::Xaml::RoutedEventArgs const&);
+    void Exit_Click(IInspectable const&, Microsoft::UI::Xaml::RoutedEventArgs const&);
 };
 }
 
